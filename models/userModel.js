149a541d93c52b86ce24c02a474a90b6b248c547
Hasan Nawaz <hncse@gmail.com>
2025-07-21 14:05:37 +0530

    Initial setup

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose, { model } from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+
+const userSchema = mongoose.Schema({
+    fullName : String,
+    email : String,
+    password : String,
+    cart : {
+        type : Array,
+        default : []
+    },
+    isAdmin : Boolean,
+    orders : {
+        type : Array,
+        default : []
+    },
+    contact : Number,
+    picture : String
+});
+
+const userModel = mongoose.model("user",userSchema);
+export default userModel;
+
